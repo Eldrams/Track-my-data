aad4399046e5a24cb33b6f353d5826e6043935de
Eldrams <126687182+Eldrams@users.noreply.github.com>
2023-05-29 15:24:31 +0100

    Updated code and refactor
    Refactored some of the code to reduce the use of event listeners.
    
    added functionality to the delete button. finds the index of the array and removes the original with array with splice.
    
    added some checks to prevent NaN error when submit is pressed without any input.
    
    added unique ID to each element to aid in removing from array and preventing accidental deletions.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Track-my-data
 Application for tracking KPI's at my sales job
+
+<!-- Description -->
+
+An application i made in order to fix an issue myself and collegues were having at work. Our IT department has been unable to fix the callback system at work for over a month despite numerous tickets. As well as our KPI's being hidden from us. I made this app in order to track my own leads and KPI's in order to improve my performance and the ease of access to information.
+
+Tech involved:
+
+HTML
+CSS
+Vanilla JavaScript
+
+GET images from unsplash RESTful API for background
+Defining a single purpose of each global callback 
+Creating and iterating over arrays created by user input
+Tracking and updating user information
+Manipulating CSS in JS
+Testing each function works independently before incorporating it as a callback
+Importing fonts from google and font awsome
+Bug testing and fixing
+Use of IDs and manipulating data based on user input
--- index.js
+++ index.js
@@ -14,10 +14,16 @@ const userScoreSb = document.getElementById('userScoreSb')
 const userScoreC = document.getElementById('userScoreC')
 const submitKpi = document.getElementById('submitKpi')
 const resetStats = document.getElementById('resetStats')
+const BtnSelector = document.getElementsByClassName('wrap') //accessing div buttons for score tracking
+const listBtn = document.getElementsByClassName('list')//access list buttons from template literal
 let dcScore = 0
 let sbScore = 0
 let cCurrent = 0
 let leadsArray = []
+
+function createId(){
+  return  Math.random()* 1000000000 // creates random number to be used as id
+}
 
 function getBackground(){
 fetch("https://api.unsplash.com/photos/random?client_id=vX0G1CIva3BADMISl-QLDPJCT7ICU4IYkoeQJnvqKJY&orientation=landscape&query=electric cars", {method: "GET", headers: {
@@ -26,15 +32,16 @@ fetch("https://api.unsplash.com/photos/random?client_id=vX0G1CIva3BADMISl-QLDPJC
   },})
     .then(res => res.json())
     .then(data => {
-        console.log(data)
         document.body.style.backgroundImage = `url("${data.urls.full}")`
     })
+    .catch(err => console.log("error"))
 }
-
+// getBackground() limited to 50 calls per hour
 
 form.addEventListener('submit', function (e){
     e.preventDefault() //prevent default submission action
     let leads = { //object template for leads array
+        uuid: parseInt(createId()), //conver to integer
         firstName: document.getElementById('firstName').value, // takes user input and adds to leadsArray
         lastName: document.getElementById('lastName').value,
         bussinessName: document.getElementById('bussinessName').value,
@@ -44,23 +51,24 @@ form.addEventListener('submit', function (e){
         callBack: document.getElementById('callBack').value.replace('T', ' Time: ') // removes the default T and replaces it with new string
     }
     leadsArray.unshift(leads) //adds to the begging of the array (most recent lead is at the top)
-    console.log(leadsArray)
     form.reset()
     render()
 })
 
-
 submitKpi.addEventListener('click', ()=>{
-    dcScore += parseInt(userScoreDc.value) // convert string input to integer
-    sbScore += parseInt(userScoreSb.value) 
-    cCurrent += parseInt(userScoreC.value)
-    render() // updates page
+    if (userInputDc.value.length == 0 || userInputSb.value.length == 0 || userInputC.value.length == 0){
+        alert('Enter current scores')
+    }else{
+        dcScore += parseInt(userInputDc.value) // convert string input to integer
+        sbScore += parseInt(userInputSb.value) 
+        cCurrent += parseInt(userInputC.value)
+        render() // updates page
+    }
 })
 
 resetStats.addEventListener('click', () => {
     window.location.reload(); // refresh page
 })
-
 
 function render(){
     dataCapture.innerHTML = `<h1>Data Captures: ${dcScore} / 60</h1>` // add a moving target for each band
@@ -69,18 +77,18 @@ function render(){
     enableDecrement() //has to take place in render so logic has updated score
     submitKpi.style.display = 'none' // hides submit button when KPIs are set
     renderList.innerHTML = '' //Clears list after each iteration (prevents duplicates)
-    leadsArray.forEach(leads => { //iterates through array each time render is called
+    leadsArray.forEach(leads => { //iterates through leads array each time render is called
         renderList.innerHTML += `
         <div id="nameContainer">
                 <h1>${leads.firstName} ${leads.lastName}</h1>
                 <h1>${leads.bussinessName}</h1> 
                 <h1>CB: ${leads.callBack}</h1>
             </div>
-            <div id="contactInfo">
+            <div class="contactInfo" id="contactInfo">
                 <h3>${leads.email}</h3>
                 <h3>No. ${leads.telephone}</h3>
-                <button id="editLead"<i class="fa-solid fa-pen-to-square"></i></button>
-                <button id="deleteLead"<i class="fa-solid fa-delete-left"></i></button>
+                <button data-edit="${leads.uuid}" class="editLead"><i class="fa-solid fa-pen-to-square"></i></button>
+                <button data-remove="${leads.uuid}" class="deleteLead"><i data-remove="${leads.uuid}" class="fa-solid fa-delete-left"></i></button>
             </div>
             <div id="comments"> 
                 <p>${leads.notes}</p>
@@ -88,52 +96,53 @@ function render(){
         <div class="spacer"></div>
         `
     })
-    document.getElementById('editLead').addEventListener('click', (e) =>{
-        console.log('hi')
-    }) 
-    document.getElementById('deleteLead').addEventListener('click', (e) =>{
-        console.log('hi 1')
-        leadsArray.indexOf(leads.firstName.value)
-    }) 
+}
+
+for (let i of listBtn){
+    i.addEventListener('click', (e) =>{
+        let itemRemove = e.target.getAttribute('data-remove')
+        let itemId = parseInt(itemRemove)
+        let removeIndex = leadsArray.map(item => item.uuid).indexOf(itemId)
+        if (removeIndex > -1){
+            leadsArray.splice(removeIndex, 1)
+            render()
+        }
+    })
 }
   
 // increment and decrementing scores
 
-decrementDc.addEventListener('click', () =>{
-    dcScore-- 
-    render()
-})
-
-
-decrementSb.addEventListener('click', () =>{
-    sbScore--
-    render()
-})
-
-decrementC.addEventListener('click', () =>{
-    cCurrent--
-    render()
-})
-
-incrementDc.addEventListener('click', () => {
-    dcScore++
-    render()
-})
-
-incrementSb.addEventListener('click', () => {
-    sbScore++
-    render()
-})
-
-incrementC.addEventListener('click', () => {
-    cCurrent++
-    render()
-})
+for (let i of BtnSelector){ //iterating through button tags
+    i.addEventListener('click', (e) => { //adding event listner to hold them all
+        let btnId = e.target.getAttribute('id'); //finding the attribut of the click event
+        if (btnId === decrementDc.id){
+            dcScore-- 
+            render()
+        }else if (btnId === decrementSb.id){
+            sbScore--
+            render()
+        }else if (btnId === decrementC.id){
+            cCurrent--
+            render()
+        }else if (btnId == incrementDc.id){
+            dcScore++
+            render()
+        }else if (btnId === incrementSb.id){
+            sbScore++
+            render()
+        }else if (btnId === incrementC.id){
+            cCurrent++
+            render()
+        }else { //To prevent a bug where clicking anywhere in the div would immediatly submit scores
+            return
+        }
+    })
+}
 
 // prevents decrementing below 0
 function enableDecrement(){
     if (dcScore === 0){
-        decrementDc.disabled = true
+        decrementDc.disabled = true //html is set as disabled, toggles based on score
     }else if (dcScore >= 1){
         decrementDc.disabled = false
     }
@@ -151,19 +160,3 @@ function enableDecrement(){
     }
 }
 
-// document.querySelectorAll('button').forEach(elem => elem.addEventListener('click', function (e){
-//    console.log(e.target.id)
-//    if (e.target.incrementDc){
-//    dcScore += 1
-//    render()
-//    }else{
-
-//    }
-// }))
-
-// dcScore = 0
-    // dataCapture.innerHTML = `<li id="dataCaptures"><input id="userScoreDc" type="number"></li>`
-    // salaryBoost.innerHTML = `<li id="salaryBoosts"><input id="userScoreSb" type="number"></li>`
-    // callCurrent.innerHTML = `<li id="callCurrent"><input id="userScoreC" type="number"></li>`
-    // submit.style.display = 'block'
-    // render()
